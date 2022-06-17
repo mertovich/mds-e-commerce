9c537dee11ed104f4698e586f0c41c7bab65b794
mertovich <88425310+mertovich@users.noreply.github.com>
2022-06-17 13:09:21 +0300

    add(Spinner)

--- web/src/ui-library/components/ProductList.tsx
+++ web/src/ui-library/components/ProductList.tsx
@@ -12,6 +12,7 @@ import {
     StatNumber,
     HStack,
     Button,
+    Spinner,
 } from '@chakra-ui/react'
 import { SearchIcon } from '@chakra-ui/icons'
 import React, { useState } from 'react'
@@ -19,72 +20,89 @@ import React, { useState } from 'react'
 type Props = {}
 
 const ProductList = (props: Props) => {
-    const [Loaded, setLoaded] = useState<boolean>(false)
+    const [Loaded, setLoaded] = useState<boolean>(true)
 
     return (
         <Box>
-                <Stack
-                    width="25%"
-                    margin="auto"
-                >
-                    <InputGroup>
-                        <InputLeftElement
-                            pointerEvents='none'
-                            children={<SearchIcon color='gray.400' />}
-                        />
-                        <Input type='tel' placeholder='Search' />
-                    </InputGroup>
-                </Stack>
-                <SimpleGrid
-                    spacing='1%'
-                    minChildWidth='300px'
-                    paddingTop={'3%'}
-                    paddingBottom={'5%'}
-                    paddingLeft={'3%'}
-                    paddingRight={'3%'}
-                >
-                    {Array.from({ length: 12 }).map((_, index) => (
-                        <Box
-                            bgColor={'gray.200'}
-                            borderRadius={'10px'}
-                        >
+            <Stack
+                width="25%"
+                margin="auto"
+            >
+                <InputGroup>
+                    <InputLeftElement
+                        pointerEvents='none'
+                        children={<SearchIcon color='gray.400' />}
+                    />
+                    <Input type='tel' placeholder='Search' />
+                </InputGroup>
+            </Stack>
+            {
+                Loaded ?
+                    <SimpleGrid
+                        spacing='1%'
+                        minChildWidth='300px'
+                        paddingTop={'3%'}
+                        paddingBottom={'5%'}
+                        paddingLeft={'3%'}
+                        paddingRight={'3%'}
+                    >
+                        {Array.from({ length: 12 }).map((_, index) => (
                             <Box
-                                width={'100%'}
-                                bgColor={'blackAlpha.100'}
+                                bgColor={'gray.200'}
+                                borderRadius={'10px'}
                             >
-                                <Image src='https://www.pngmart.com/files/21/Accent-Chair-PNG-Photos.png' height={'300px'} />
+                                <Box
+                                    width={'100%'}
+                                    bgColor={'blackAlpha.100'}
+                                >
+                                    <Image src='https://www.pngmart.com/files/21/Accent-Chair-PNG-Photos.png' height={'300px'} />
+                                </Box>
+                                <Text
+                                    fontSize={'xl'}
+                                    textAlign={'center'}
+                                    padding={5}
+                                >Chair Model X
+                                </Text>
+                                <Stat
+                                    textAlign={'center'}
+                                >
+                                    <StatLabel>selle name</StatLabel>
+                                    <StatNumber>£0.00</StatNumber>
+                                </Stat>
+                                <HStack
+                                    justifyContent={'center'}
+                                >
+                                    <Button
+                                        colorScheme={'green'}
+                                        variant='solid'
+                                        margin={2}
+                                    >Buy
+                                    </Button>
+                                    <Button
+                                        colorScheme={'cyan'}
+                                        variant='outline'
+                                        margin={2}
+                                    >add to Basket
+                                    </Button>
+                                </HStack>
                             </Box>
-                            <Text
-                                fontSize={'xl'}
-                                textAlign={'center'}
-                                padding={5}
-                            >Chair Model X
-                            </Text>
-                            <Stat
-                                textAlign={'center'}
-                            >
-                                <StatLabel>selle name</StatLabel>
-                                <StatNumber>£0.00</StatNumber>
-                            </Stat>
-                            <HStack
-                                justifyContent={'center'}
-                            >
-                                <Button
-                                    colorScheme={'green'}
-                                    variant='solid'
-                                    margin={2}
-                                >Buy
-                                </Button>
-                                <Button
-                                    colorScheme={'cyan'}
-                                    variant='outline'
-                                    margin={2}
-                                >add to Basket
-                                </Button>
-                            </HStack>
-                        </Box>
-                    ))}
-                </SimpleGrid>
+                        ))}
+                    </SimpleGrid>
+                    :
+                    <Box
+                            textAlign={'center'}
+                            marginTop={'5%'}
+                    >
+                        <Spinner
+                            thickness='4px'
+                            speed='0.65s'
+                            emptyColor='gray.200'
+                            color='blue.500'
+                            size='xl'
+                        />
+                    </Box>
+            }
+
         </Box>
     )
 }
