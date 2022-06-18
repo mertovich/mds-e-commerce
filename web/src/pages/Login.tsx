64cf7e33e08836228a379eb076af46351b910ae0
mertovich <88425310+mertovich@users.noreply.github.com>
2022-06-18 08:44:24 +0300

    add(LoginPage)

--- web/src/pages/Login.tsx
+++ web/src/pages/Login.tsx
@@ -1,10 +1,23 @@
+import {
+  Box,
+} from '@chakra-ui/react'
 import React from 'react'
+import LoginForm from '../ui-library/components/LoginForm'
 
 type Props = {}
 
 const Login = (props: Props) => {
   return (
-    <div>Login</div>
+    <Box
+      display={'flex'}
+      flexDirection={'column'}
+      alignItems={'center'}
+      justifyContent={'center'}
+      height={'100vh'}
+      bgColor={'gray.100'}
+    >
+     <LoginForm/>
+    </Box>
   )
 }
 
