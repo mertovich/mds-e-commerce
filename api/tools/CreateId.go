d866f43702a319783e3c730e850d508f4182af4c
mertovich <88425310+mertovich@users.noreply.github.com>
2022-06-20 16:33:04 +0300

    add(tools): Create id

---
+++ api/tools/CreateId.go
@@ -0,0 +1,18 @@
+package tools
+
+import "github.com/google/uuid"
+
+func CreateId(acctype string) string {
+	// Create a new UUID
+	uuid := uuid.NewString()
+
+	if acctype == "Customer" {
+		return "1" + uuid
+	} else if acctype == "Company" {
+		return "2" + uuid
+	} else {
+		return ""
+	}
+
+	return uuid
+}
