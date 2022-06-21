86f2a1ec6082dd210804803e6f47ebf09a38bdc1
mertovich <88425310+mertovich@users.noreply.github.com>
2022-06-21 11:39:01 +0300

    add(handler): login handler

--- api/handlers/userManagerHandler.go
+++ api/handlers/userManagerHandler.go
@@ -70,3 +70,35 @@ func Register(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, string(tokenJson))
 	}
 }
+
+func Login(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+
+	if r.URL.Path != "/api/login" {
+		http.NotFound(w, r)
+		return
+	}
+
+	type User struct {
+		Email    string `json:"email"`
+		Password string `json:"password"`
+	}
+
+	bodyByte, _ := ioutil.ReadAll(r.Body)
+	var user User
+	json.Unmarshal(bodyByte, &user)
+
+	token, err := datamanager.Login(user.Email, user.Password)
+	if err != nil {
+		fmt.Fprintf(w, "Error: %s", err)
+		return
+	}
+
+	tokenMap := map[string]string{
+		"token": token,
+	}
+	tokenJson, _ := json.Marshal(tokenMap)
+	fmt.Fprintf(w, string(tokenJson))
+
+}
