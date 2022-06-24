3ef0508969ba748d2df940c120e247a46d2bb194
mertovich <88425310+mertovich@users.noreply.github.com>
2022-06-24 12:17:17 +0300

    add(Login): auth navigate

--- web/src/pages/Login.tsx
+++ web/src/pages/Login.tsx
@@ -1,12 +1,32 @@
 import {
   Box,
 } from '@chakra-ui/react'
-import React from 'react'
+import React,{useEffect} from 'react'
 import LoginForm from '../ui-library/components/LoginForm'
+import {useNavigate} from 'react-router-dom'
+import {authValidation} from '../auth/index'
 
 type Props = {}
 
 const Login = (props: Props) => {
+  const navigate = useNavigate()
+
+  useEffect(() => {
+    authNavigate()
+  }, [])
+
+  const authNavigate = async () => {
+    const token = localStorage.getItem('token')
+    if(token) {
+      const control = await authValidation(token)
+      if(control) {
+        navigate('/')
+      }
+    } else {
+      navigate('/login')
+    }
+  }
+
   return (
     <Box
       display={'flex'}
