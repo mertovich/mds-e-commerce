b7c132f4af07f3f1f7c678445a63847d227faf1e
mertovich <88425310+mertovich@users.noreply.github.com>
2022-06-28 08:06:48 +0300

    add(CompanyModel): product history

--- api/models/company.go
+++ api/models/company.go
@@ -1,12 +1,13 @@
 package models
 
 type Company struct {
-	ID        string    `json:"id"`
-	Name      string    `json:"name"`
-	Surname   string    `json:"surname"`
-	Password  string    `json:"password"`
-	Email     string    `json:"email"`
-	Products  []Product `json:"products"`
-	CreatedAt string    `json:"created_at"`
-	Token     string    `json:"token"`
+	ID              string    `json:"id"`
+	Name            string    `json:"name"`
+	Surname         string    `json:"surname"`
+	Password        string    `json:"password"`
+	Email           string    `json:"email"`
+	Products        []Product `json:"products"`
+	PurchaseHistory []Product `json:"purchase_history"`
+	CreatedAt       string    `json:"created_at"`
+	Token           string    `json:"token"`
 }
