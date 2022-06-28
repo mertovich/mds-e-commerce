5d951c84ac7ac689415e324536782148e244f2c8
mertovich <88425310+mertovich@users.noreply.github.com>
2022-06-28 09:26:33 +0300

    add(HistoryList)

---
+++ web/src/ui-library/components/HistoryList.tsx
@@ -0,0 +1,72 @@
+import {
+    Box,
+    TableContainer,
+    Table,
+    TableCaption,
+    Thead,
+    Tr,
+    Th,
+    Tbody,
+    Td,
+} from '@chakra-ui/react'
+import React, { useState, useEffect } from 'react'
+
+type Props = {}
+
+const HistoryList = (props: Props) => {
+    const [user, setUser] = useState(JSON.parse(localStorage.getItem('user') || '{}'))
+    const [history, setHistory] = useState([])
+
+    useEffect(() => {
+        getPurchaseHistory()
+    }, [])
+
+
+    const getPurchaseHistory = () => {
+        const requestOptions = {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({
+                id: user.id,
+                token: localStorage.getItem('token')
+            })
+        };
+        fetch('http://localhost:8080/api/customer/purchase-history', requestOptions)
+            .then(response => response.json())
+            .then(data => {
+                setHistory(data.purchase_history)
+            })
+    }
+
+
+    return (
+        <Box>
+            <TableContainer>
+                <Table variant={'striped'} colorScheme='blue'>
+                    <TableCaption>Purchase History</TableCaption>
+                    <Thead>
+                        <Tr>
+                            <Th>Product Name</Th>
+                            <Th>Seller</Th>
+                            <Th isNumeric>Price</Th>
+                        </Tr>
+                    </Thead>
+                    <Tbody>
+                        {history.map((item: any, index) => {
+                            return (
+                                <Tr key={index}>
+                                    <Td>{item.name}</Td>
+                                    <Td>{item.seller}</Td>
+                                    <Td isNumeric>{item.price}</Td>
+                                </Tr>
+                            )
+                        }
+                        )}
+                    </Tbody>
+                </Table>
+            </TableContainer>
+        </Box>
+    )
+}
+
+export default HistoryList
