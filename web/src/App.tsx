8d97e6f85ce7b654560c24ef01c622ac84f301a9
mertovich <88425310+mertovich@users.noreply.github.com>
2022-06-28 09:26:03 +0300

    add(Route): purchase history

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -4,6 +4,7 @@ import Home from './/pages/Home'
 import Login from './pages/Login'
 import Register from './pages/Register'
 import Settings from './pages/Settings'
+import PurchaseHistory from './pages/PurchaseHistory'
 
 type Props = {}
 
@@ -15,6 +16,7 @@ const App = (props: Props) => {
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
         <Route path="/settings" element={<Settings />} />
+        <Route path="/purchase-history" element={<PurchaseHistory />} />
       </Routes>
     </div>
   )
