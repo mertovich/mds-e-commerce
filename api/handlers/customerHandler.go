f8351a47c761913395419f2552c6c942b9bed7d2
mertovich <88425310+mertovich@users.noreply.github.com>
2022-06-28 09:27:33 +0300

    add(customerHandler): customer purchase history

--- api/handlers/customerHandler.go
+++ api/handlers/customerHandler.go
@@ -45,3 +45,34 @@ func UpdateCustomer(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 }
+
+func CustomerPurchaseHistory(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+
+	if r.URL.Path != "/api/customer/purchase-history" {
+		http.NotFound(w, r)
+		return
+	}
+
+	type User struct {
+		ID    string `json:"id"`
+		Token string `json:"token"`
+	}
+	bodyByte, _ := ioutil.ReadAll(r.Body)
+	var user User
+	json.Unmarshal(bodyByte, &user)
+	authValid := auth.Auth(user.Token)
+	if authValid == true {
+		purchaseHistory := datamanager.GetCustomerPurchaseHistory(user.ID)
+		maps := map[string]interface{}{"message": "Success", "purchase_history": purchaseHistory}
+		mapsJson, _ := json.Marshal(maps)
+		fmt.Fprintf(w, string(mapsJson))
+	} else {
+		fmt.Println("Invalid token")
+		maps := map[string]string{"message": "Invalid token"}
+		mapsJson, _ := json.Marshal(maps)
+		fmt.Fprintf(w, string(mapsJson))
+		return
+	}
+}
