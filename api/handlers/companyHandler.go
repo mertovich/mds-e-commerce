3283a0145361a8e22697beae44256e76dfd8e5ad
mertovich <88425310+mertovich@users.noreply.github.com>
2022-06-30 11:47:15 +0300

    add(handlers): Company handler

--- api/handlers/companyHandler.go
+++ api/handlers/companyHandler.go
@@ -3,10 +3,13 @@ package handlers
 import (
 	"api/auth"
 	"api/datamanager"
+	"api/models"
+	"api/tools"
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"net/http"
+	"time"
 )
 
 func CompanyPurchaseHistory(w http.ResponseWriter, r *http.Request) {
@@ -37,5 +40,36 @@ func CompanyPurchaseHistory(w http.ResponseWriter, r *http.Request) {
 		mapsJson, _ := json.Marshal(maps)
 		fmt.Fprintf(w, string(mapsJson))
 		return
+	}
+}
+
+func CompanyAddProduct(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+
+	if r.URL.Path != "/api/company/add-product" {
+		http.NotFound(w, r)
+		return
+	}
+
+	p := models.Product{}
+	bodyByte, _ := ioutil.ReadAll(r.Body)
+	json.Unmarshal(bodyByte, &p)
+	if p.SellerId != "" {
+		p.ID = tools.CreateProductId()
+		p.CreatedAt = time.Now().String()
+		p.Comments = []models.Comment{}
+
+		fmt.Printf("id: %s\n", p.ID)
+		fmt.Printf("Createat: %s\n", p.CreatedAt)
+		fmt.Printf("name: %s\n", p.Name)
+		fmt.Printf("des: %s\n", p.Description)
+		fmt.Printf("price: %d\n", p.Price)
+		fmt.Printf("sellerId: %s\n", p.SellerId)
+		fmt.Printf("image: %s\n", p.Image)
+		fmt.Printf("category: %s\n", p.Category)
+		fmt.Printf("seller: %s\n", p.Seller)
+
+		datamanager.CompanyAddProduct(p)
 	}
 }
