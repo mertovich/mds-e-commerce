a0e6fa871c11fa3b913e8434e608be448ffe8f89
mertovich <88425310+mertovich@users.noreply.github.com>
2022-06-30 11:47:37 +0300

    add(models): Product model add seller id

--- api/models/product.go
+++ api/models/product.go
@@ -3,6 +3,7 @@ package models
 type Product struct {
 	ID          string    `json:"id"`
 	Name        string    `json:"name"`
+	SellerId    string    `json:"seller_id"`
 	Seller      string    `json:"seller"`
 	Image       string    `json:"image"`
 	Description string    `json:"description"`
