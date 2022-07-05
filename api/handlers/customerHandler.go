244d4605fad9b73f58dad30b5258e62df053dd04
mertovich <88425310+mertovich@users.noreply.github.com>
2022-07-05 14:27:05 +0300

    add(handlers): customer product buy

--- api/handlers/customerHandler.go
+++ api/handlers/customerHandler.go
@@ -3,13 +3,14 @@ package handlers
 import (
 	"api/auth"
 	"api/datamanager"
+	"api/models"
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"net/http"
 )
 
-func CustomerPurchaseHistory(w http.ResponseWriter, r *http.Request) {
+func CustomerPurchaseHistoryGet(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Access-Control-Allow-Origin", "*")
 	w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
 
@@ -38,4 +39,39 @@ func CustomerPurchaseHistory(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, string(mapsJson))
 		return
 	}
+}
+
+func CustomerProductPost(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+
+	if r.URL.Path != "/api/customer/product-buy" {
+		http.NotFound(w, r)
+		return
+	}
+
+	type User struct {
+		ID      string         `json:"id"`
+		Token   string         `json:"token"`
+		Product models.Product `json:"product"`
+	}
+
+	var user User
+	bodyByte, _ := ioutil.ReadAll(r.Body)
+	json.Unmarshal(bodyByte, &user)
+
+	authValid := auth.Auth(user.Token)
+	if authValid == true {
+		datamanager.AddProductHistoryCustomer(user.Product, user.ID)
+		maps := map[string]string{"message": "Success"}
+		mapsJson, _ := json.Marshal(maps)
+		fmt.Fprintf(w, string(mapsJson))
+	} else {
+		fmt.Println("Invalid token")
+		maps := map[string]string{"message": "Invalid token"}
+		mapsJson, _ := json.Marshal(maps)
+		fmt.Fprintf(w, string(mapsJson))
+		return
+	}
+
 }
