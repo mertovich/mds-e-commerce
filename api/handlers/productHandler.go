e72f094bd727d799899b366460f20f774cb9d6a2
mertovich <88425310+mertovich@users.noreply.github.com>
2022-07-07 14:30:52 +0200

    add(handlers): get product id

--- api/handlers/productHandler.go
+++ api/handlers/productHandler.go
@@ -4,6 +4,7 @@ import (
 	"api/datamanager"
 	"encoding/json"
 	"fmt"
+	"log"
 	"net/http"
 )
 
@@ -19,4 +20,24 @@ func GetAllProducts(w http.ResponseWriter, r *http.Request) {
 	products := datamanager.GetProducts()
 	productsJSON, _ := json.Marshal(products)
 	fmt.Fprintf(w, string(productsJSON))
+}
+
+func GetProductId(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+
+	keys, ok := r.URL.Query()["id"]
+
+	if !ok || len(keys[0]) < 1 {
+		log.Println("Url Param 'id' is missing")
+		return
+	}
+
+	// Query()["key"] will return an array of items,
+	// we only want the single item.
+	key := keys[0]
+
+	product := datamanager.GetProductId(key)
+	productJSON, _ := json.Marshal(product)
+	fmt.Fprintf(w, string(productJSON))
 }
