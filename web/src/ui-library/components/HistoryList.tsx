02752df0f9cde4a5c4d47fb886958935a001ca22
mertovich <88425310+mertovich@users.noreply.github.com>
2022-07-08 11:23:41 +0200

    add(HistoryList): product comment add

--- web/src/ui-library/components/HistoryList.tsx
+++ web/src/ui-library/components/HistoryList.tsx
@@ -8,6 +8,19 @@ import {
     Th,
     Tbody,
     Td,
+    Button,
+    useDisclosure,
+    Modal,
+    ModalOverlay,
+    ModalContent,
+    ModalHeader,
+    ModalCloseButton,
+    ModalBody,
+    FormControl,
+    FormLabel,
+    Textarea,
+    ModalFooter,
+    useToast,
 } from '@chakra-ui/react'
 import React, { useState, useEffect } from 'react'
 
@@ -16,6 +29,11 @@ type Props = {}
 const HistoryList = (props: Props) => {
     const [user, setUser] = useState(JSON.parse(localStorage.getItem('user') || '{}'))
     const [history, setHistory] = useState([])
+    const [comment, setComment] = useState<string>('')
+    const [product, setProduct] = useState<any>({})
+
+    const { isOpen, onOpen, onClose } = useDisclosure()
+    const toast = useToast()
 
     const config = require('../../config.json')
 
@@ -23,12 +41,53 @@ const HistoryList = (props: Props) => {
         getPurchaseHistory()
     }, [])
 
+    const toastMessage = (title: string, message: string, statusType: any = 'error', durationValue: number=9000,positionValue:any='top-right') => {
+        toast({
+            title: title,
+            description: message,
+            position: positionValue,
+            status: statusType,
+            duration: durationValue,
+            isClosable: true,
+        })
+    }
+
+    const addCommentButton = () => {
+        const requestOptions = {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({
+                token: localStorage.getItem('token'),
+                product_id: product.id,
+                comment: {
+                    customer_id: user.id,
+                    name: user.name,
+                    surname: user.surname,
+                    description: comment,
+                }
+            }),
+        }
+        fetch(`${config.api_url}/api/product-comment`, requestOptions)
+            .then(response => response.json())
+            .then(data => {
+                if(data.message === 'success'){
+                    toastMessage('Success', 'Comment added successfully', 'success',3000,'bottom-right')
+                    onClose()
+                }
+            }
+            )
+    }
+
+    const addCommentAlert = (product:any) => {
+        onOpen()
+        setProduct(product)
+    }
 
     const getPurchaseHistory = async () => {
         let userType = ''
-        if(user.id[0] === '1'){
+        if (user.id[0] === '1') {
             userType = 'customer'
-        } else if(user.id[0] === '2'){
+        } else if (user.id[0] === '2') {
             userType = 'company'
         }
 
@@ -57,6 +116,7 @@ const HistoryList = (props: Props) => {
                             <Th>Product Name</Th>
                             <Th>Seller</Th>
                             <Th isNumeric>Price</Th>
+                            <Th></Th>
                         </Tr>
                     </Thead>
                     <Tbody>
@@ -66,6 +126,17 @@ const HistoryList = (props: Props) => {
                                     <Td>{item.name}</Td>
                                     <Td>{item.seller}</Td>
                                     <Td isNumeric>{item.price}</Td>
+                                    <Td
+                                        textAlign={'center'}
+                                    >
+                                        <Button
+                                            size={'sm'}
+                                            onClick={() => addCommentAlert(item)}
+                                            colorScheme={"orange"}
+                                        >
+                                            Add Comment
+                                        </Button>
+                                    </Td>
                                 </Tr>
                             )
                         }
@@ -73,6 +144,35 @@ const HistoryList = (props: Props) => {
                     </Tbody>
                 </Table>
             </TableContainer>
+            <Modal
+                isOpen={isOpen}
+                onClose={onClose}
+            >
+                <ModalOverlay />
+                <ModalContent>
+                    <ModalHeader>Create your comment</ModalHeader>
+                    <ModalCloseButton />
+                    <ModalBody pb={6}>
+                        <FormControl mt={4}>
+                            <FormLabel>Comment</FormLabel>
+                            <Textarea
+                                placeholder='write your comment here'
+                                onChange={(e) => setComment(e.target.value)}
+                            />
+                        </FormControl>
+                    </ModalBody>
+
+                    <ModalFooter>
+                        <Button 
+                        colorScheme='blue' mr={3}
+                        onClick={addCommentButton}
+                        >
+                            Save
+                        </Button>
+                        <Button onClick={onClose}>Cancel</Button>
+                    </ModalFooter>
+                </ModalContent>
+            </Modal>
         </Box>
     )
 }
