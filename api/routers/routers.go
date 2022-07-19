5391ca569ed086816f86601de81eacb11ea0a670
mertovich <88425310+mertovich@users.noreply.github.com>
2022-07-19 13:15:35 +0200

    update(routers): code edit

--- api/routers/routers.go
+++ api/routers/routers.go
@@ -6,18 +6,26 @@ import (
 )
 
 func Inıt() {
+	// user routers
 	http.HandleFunc("/api/register", handlers.Register)
 	http.HandleFunc("/api/login", handlers.Login)
 	http.HandleFunc("/api/auth", handlers.Auth)
 	http.HandleFunc("/api/user/update", handlers.UpdateUser)
-	http.HandleFunc("/api/customer/purchase-history", handlers.CustomerPurchaseHistoryGet)
-	http.HandleFunc("/api/company/purchase-history", handlers.CompanyPurchaseHistory)
-	http.HandleFunc("/api/company/add-product", handlers.CompanyAddProduct)
+
+	// product routers
 	http.HandleFunc("/api/products", handlers.GetAllProducts)
-	http.HandleFunc("/api/customer/product-buy", handlers.CustomerProductBuy)
-	http.HandleFunc("/api/company/product-buy", handlers.CompanyProductBuy)
 	http.HandleFunc("/api/product/", handlers.GetProductId)
 	http.HandleFunc("/api/product-comment", handlers.AddProductComment)
-	http.HandleFunc("/api/company/product-list", handlers.CompanyProductList)
 	http.HandleFunc("/api/product-remove", handlers.RemoveItemProductList)
+
+	// company routers
+	http.HandleFunc("/api/company/purchase-history", handlers.CompanyPurchaseHistory)
+	http.HandleFunc("/api/company/add-product", handlers.CompanyAddProduct)
+	http.HandleFunc("/api/company/product-buy", handlers.CompanyProductBuy)
+	http.HandleFunc("/api/company/product-list", handlers.CompanyProductList)
+
+	// customer routers
+	http.HandleFunc("/api/customer/purchase-history", handlers.CustomerPurchaseHistoryGet)
+	http.HandleFunc("/api/customer/product-buy", handlers.CustomerProductBuy)
+
 }
